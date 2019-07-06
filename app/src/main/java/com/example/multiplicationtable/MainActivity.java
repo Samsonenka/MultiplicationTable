aa207a25d85a6c65775a3b3afaf5b447daea0d92
Pavel Samsonenka <pavel.samsonenka92@gmail.com>
2019-07-06 19:34:42 +0200

    create seekBar logic

--- app/src/main/java/com/example/multiplicationtable/MainActivity.java
+++ app/src/main/java/com/example/multiplicationtable/MainActivity.java
@@ -2,12 +2,65 @@ package com.example.multiplicationtable;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SeekBar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int max = 10;
+    private int min = 1;
+    private int count = 10;
+
+    private List<Integer> numberList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        ListView listViewNumbers = findViewById(R.id.listViewNumbers);
+
+        seekBar.setMax(max);
+
+        numberList = new ArrayList<>();
+        final ArrayAdapter<Integer> arrayAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, numberList);
+        listViewNumbers.setAdapter(arrayAdapter);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                numberList.clear();
+
+                if (progress < min){
+                    seekBar.setProgress(min);
+                }
+
+                for (int i = min; i <= count; i++) {
+                    numberList.add(seekBar.getProgress() * i);
+                }
+                arrayAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seekBar.setProgress(1);
+
+
     }
 }
